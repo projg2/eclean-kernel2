53e4960853927f4f039d5c5a8d6e00788a448065
Michał Górny <mgorny@gentoo.org>
2016-07-07 08:31:04 +0200

    Add dummy --list-kernels action

--- src/main.cxx
+++ src/main.cxx
@@ -15,23 +15,36 @@ extern "C"
 #	include <getopt.h>
 };
 
-static const char* short_options = "hV";
+static const char* short_options = "lhV";
 static const struct option long_options[] = {
+	{ "list-kernels", no_argument, nullptr, 'l' },
+
 	{ "help", no_argument, nullptr, 'h' },
 	{ "version", no_argument, nullptr, 'V' },
+
 	{ nullptr, no_argument, nullptr, 0 },
 };
 
 static void print_help(std::ostream& out, const char* argv0)
 {
-	out << "Usage: " << argv0 << " [options]\n"
+	out << "Usage: " << argv0 << " [options] <action>\n"
+		"Actions:\n"
+		"  -l, --list-kernels      list installed kernels\n"
 		"Options:\n"
 		"  -h, --help              print this help message\n"
 		"  -V, --version           print program version\n";
 }
 
+enum class Action
+{
+	none,
+	list_kernels,
+};
+
 int main(int argc, char* argv[])
 {
+	Action act = Action::none;
+
 	while (true)
 	{
 		int opt = getopt_long(argc, argv, short_options, long_options,
@@ -41,6 +54,15 @@ int main(int argc, char* argv[])
 
 		switch (opt)
 		{
+			case 'l':
+				if (act != Action::none)
+				{
+					std::cerr << argv[0] << ": multiple actions specified\n";
+					return 1;
+				}
+				act = Action::list_kernels;
+				break;
+
 			case 'h':
 				print_help(std::cout, argv[0]);
 				return 0;
@@ -51,6 +73,12 @@ int main(int argc, char* argv[])
 				print_help(std::cerr, argv[0]);
 				return 1;
 		}
+	}
+
+	if (act == Action::none)
+	{
+		std::cerr << argv[0] << ": no action specified\n";
+		return 1;
 	}
 
 	if (optind != argc)
